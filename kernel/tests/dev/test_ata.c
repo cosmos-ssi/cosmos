6c51777c89469e9049062fc05e2fae3e781f9661
Tom Everett <tom@khubla.com>
2021-02-05 16:37:43 -0700

    simplify test_ata
    we have a block device test helper now…

--- kernel/tests/dev/test_ata.c
+++ kernel/tests/dev/test_ata.c
@@ -14,62 +14,14 @@
 #include <sys/string/mem.h>
 #include <sys/string/string.h>
 #include <tests/dev/test_ata.h>
+#include <tests/dev/test_blockdevice.h>
 
 void test_ata1() {
-    // get virtual block device
-    uint8_t devicename[] = {"disk0"};
+    uint8_t devicename[] = {"disk3"};
 
     struct device* ata = devicemgr_find_device(devicename);
     if (0 != ata) {
-        struct deviceapi_block* ata_api = (struct deviceapi_block*)ata->api;
-
-        uint16_t sector_size = (*ata_api->sector_size)(ata);
-        kprintf("Sector size %llu\n", sector_size);
-
-        uint32_t total_size = (*ata_api->total_size)(ata);
-        kprintf("Total size %llu\n", total_size);
-
-        uint32_t buffersize = sizeof(uint8_t) * sector_size;
-
-        uint8_t* data = kmalloc(buffersize);
-        memset(data, 0, buffersize);
-
-        (*ata_api->read)(ata, 0, data, 1);  // 1 sector
-        debug_show_memblock(data, sector_size);
-    } else {
-        kprintf("Unable to find %s\n", devicename);
-    }
-}
-
-void test_ata2() {
-    // get virtual block device
-    uint8_t devicename[] = {"disk0"};
-
-    struct device* ata = devicemgr_find_device(devicename);
-    if (0 != ata) {
-        struct deviceapi_block* ata_api = (struct deviceapi_block*)ata->api;
-
-        uint16_t sector_size = (*ata_api->sector_size)(ata);
-        kprintf("Sector size %llu\n", sector_size);
-
-        uint32_t total_size = (*ata_api->total_size)(ata);
-        kprintf("Total size %llu\n", total_size);
-
-        uint32_t buffersize = sizeof(uint8_t) * sector_size;
-
-        uint8_t* outdata = kmalloc(buffersize);
-        memset(outdata, 0x11, buffersize);
-
-        // write
-        //	debug_show_memblock(outdata, sector_size);
-        (*ata_api->write)(ata, 0, outdata, 1);  // 1 sector
-
-        // read
-        uint8_t* indata = kmalloc(buffersize);
-        memset(indata, 0, buffersize);
-        (*ata_api->read)(ata, 0, indata, 1);  // 1 sector
-
-        debug_show_memblock(indata, sector_size);
+        test_block_device(ata);
     } else {
         kprintf("Unable to find %s\n", devicename);
     }
@@ -84,7 +36,6 @@ void test_ata_dma() {
 }
 
 void test_ata() {
-    //test_ata1();
-    //	test_ata2();
+    test_ata1();
     test_ata_dma();
 }
