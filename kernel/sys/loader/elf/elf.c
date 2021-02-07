497af01ae29b44b92b75dea5975ee1fabb55d089
Tom Everett <tom@khubla.com>
2021-02-07 13:34:42 -0700

    minor stuff

--- kernel/sys/loader/elf/elf.c
+++ kernel/sys/loader/elf/elf.c
@@ -6,6 +6,7 @@
 // ****************************************************************
 
 #include <sys/debug/assert.h>
+#include <sys/kprintf/kprintf.h>
 #include <sys/loader/elf/elf.h>
 
 uint8_t ELF_MAGIC[] = {0x7F, 'E', 'L', 'F'};
@@ -14,11 +15,19 @@ uint8_t elf_is_elf_binary(uint8_t* binary, uint32_t len) {
     ASSERT_NOT_NULL(binary);
     ASSERT(0 != len);
 
+    /*
+    * check header
+    */
     struct elf_header* header = (struct elf_header*)binary;
     for (uint8_t i = 0; i < 4; i++) {
         if (header->e_ident[i] != ELF_MAGIC[i]) {
+            // it's not ELF
             return 0;
         }
     }
+    /*
+    * check binary type
+    */
+    //  kprintf("type %llu\n", header->e_type);
     return 1;
 }
