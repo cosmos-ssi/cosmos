14394e4bbeaa8430500a79f32eee8d9ae494e334
Kurt M. Weber <weberk294@gmail.com>
2021-02-07 12:11:07 -0600

    syscall.h header

---
+++ kernel/sys/x86-64/syscall/syscall.h
@@ -0,0 +1,11 @@
+/*****************************************************************
+ * This file is part of CosmOS                                   *
+ * Copyright (C) 2021 Kurt M. Weber                              *
+ * Released under the stated terms in the file LICENSE           *
+ * See the file "LICENSE" in the source distribution for details *
+ *****************************************************************/
+
+#ifndef _SYSCALL_H
+#define _SYSCALL_H
+
+#endif
