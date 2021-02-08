da80758c95028c5855e7251ebe1af2f6f45751e4
Kurt M. Weber <weberk294@gmail.com>
2021-02-08 12:30:46 -0600

    process table definition

--- kernel/sys/proc/proc.h
+++ kernel/sys/proc/proc.h
@@ -4,3 +4,17 @@
  * Released under the stated terms in the file LICENSE           *
  * See the file "LICENSE" in the source distribution for details *
  *****************************************************************/
+
+#include <sys/asm/asm.h>
+
+typedef struct proc_info {
+    uint64_t id;
+    pttentry cr3;
+    proc_register rsp;
+    proc_register rbp;
+    proc_register rip;
+} proc_info;
+
+typedef proc_info******** ptable;
+
+ptable proc_table;
