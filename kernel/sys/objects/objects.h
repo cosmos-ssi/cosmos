29d8cf477aaeb985675b1303fbc4e59ce2bda0a7
Kurt M. Weber <weberk294@gmail.com>
2021-02-10 20:54:45 -0600

    initialize object_table

--- kernel/sys/objects/objects.h
+++ kernel/sys/objects/objects.h
@@ -21,6 +21,14 @@ typedef uint64_t object_handle_t;
 
 typedef object_handle_t obj_executable_t;
 
+typedef struct object_t {
+    object_types_t type;
+    void* data;
+} object_t;
+
+extern object_t** object_table;
+extern uint64_t object_table_last_idx, object_table_dim;
+
 // object_create.c
 object_handle_t object_create(object_types_t type, void* object_data);
 
