e9ecd786444c25e1f88723e6c187964e5dbba09e
Kurt M. Weber <weberk294@gmail.com>
2021-02-16 14:48:52 -0600

    scheduler_task_t struct

--- src/kernel/sys/sched/sched.h
+++ src/kernel/sys/sched/sched.h
@@ -7,7 +7,28 @@
 
 #ifndef _SCHED_H
 #define _SCHED_H
+
+#include <sys/proc/proc.h>
 #include <types.h>
+
+typedef enum scheduler_state_t {
+    SCHED_RUNNING,
+    SCHED_SLEEPING,
+    SCHED_IOWAIT,
+    SCHED_ZOMBIE  // In your he-ead, in your he-e-e-ead...
+} scheduler_state_t;
+
+typedef struct scheduler_task_t {
+    pid_t pid;
+    scheduler_state_t state;
+    uint64_t exit_code;
+
+    /* If, when going through the task list deciding which task to do next, we
+     * decide not to switch to this one for any reason, increment times_skipped.
+     * Reset to zero whenever this task IS the one switched to.
+     */
+    uint64_t times_skipped;
+} scheduler_task_t;
 
 // one each for each processor/core combo
 extern uint64_t** current_task;
