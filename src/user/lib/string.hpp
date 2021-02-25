49187225e75b65836e97140e072a17d0604e6d0f
Tom Everett <tom@khubla.com>
2021-02-24 18:30:25 -0700

    string

--- src/user/lib/string.hpp
+++ src/user/lib/string.hpp
@@ -20,6 +20,15 @@ class String {
     const uint64_t getSize();
     const uint8_t* getData();
 
+    uint64_t strlen(const uint8_t* s);
+    uint8_t* strtrim(const uint8_t* s);
+    uint8_t* strncpy(uint8_t* dest, const uint8_t* src, uint64_t len);
+    uint8_t* strncat(uint8_t* dest, const uint8_t* src, uint64_t len);
+    uint8_t strcmp(const uint8_t* str1, const uint8_t* str2);
+    uint8_t strncmp(const uint8_t* str1, const uint8_t* str2, uint64_t len);
+    uint32_t strstr(const uint8_t* str1, uint32_t start, const uint8_t* str2);
+    uint8_t* substr(const uint8_t* str1, uint32_t start, uint32_t end, uint8_t* str2, uint32_t size);
+
   private:
     uint64_t sz;
     uint8_t* m_data;
