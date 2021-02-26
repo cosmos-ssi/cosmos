1a771d9911bacf0e6f19adf0c4762b27b44740cf
Tom Everett <tom@khubla.com>
2021-02-26 14:29:02 -0700

    linkage works

--- src/user/apps/init/init.cpp
+++ src/user/apps/init/init.cpp
@@ -18,15 +18,15 @@
 * we are in user-land context here.  This is the entry point to userland that the kernel code calls
 */
 void init() {
-    //   SerialDevice* serialDevice = (SerialDevice*)Device::find(INIT_SERIAL_DEVICE);
-    //  if (0 != serialDevice) {
-    //      serialDevice->writeln("Hello from user land");
-    //  }
+    SerialDevice* serialDevice = (SerialDevice*)Device::find(INIT_SERIAL_DEVICE);
+    if (0 != serialDevice) {
+        serialDevice->writeln("Hello from user land");
+    }
 
-    //  FilesystemDevice* filesystemDevice = (FilesystemDevice*)Device::find(INIT_ROOT_FS_DEVICE);
-    //   if (0 != filesystemDevice) {
-    //       serialDevice->writeln("Found root file system");
-    //   }
+    FilesystemDevice* filesystemDevice = (FilesystemDevice*)Device::find(INIT_ROOT_FS_DEVICE);
+    if (0 != filesystemDevice) {
+        serialDevice->writeln("Found root file system");
+    }
     // guess we're done...
     //  syscall_exit();
     // here we mount a disk, I presume, and start reading files.
