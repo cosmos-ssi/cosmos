7a4d8838e4a4116ecaa7dfd3942b5d23079236cf
Kurt M. Weber <weberk294@gmail.com>
2021-03-04 16:17:22 -0600

    make sched_switch load for testing purposes

--- src/kernel/cosmos.c
+++ src/kernel/cosmos.c
@@ -157,6 +157,9 @@ void CosmOS() {
     test_process = object_process_create(test_exe);
     test_task = object_task_create(test_process);
 
+    // we never get here currently... well.... eventually the telnet over serial needs to be a on a thread
+    sched_switch(task_select());
+
     /*
     * start telnet
     */
@@ -171,9 +174,6 @@ void CosmOS() {
     } else {
         kprintf("Unable to find telnet0\n");
     }
-
-    // we never get here currently... well.... eventually the telnet over serial needs to be a on a thread
-    sched_switch(task_select());
 }
 
 filesystem_node_t* load_test_binary() {
