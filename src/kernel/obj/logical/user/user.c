9cbb0098fe9330f9c7254bd87ef4b9f8c797e300
Tom Everett <tom@khubla.com>
2021-03-05 16:34:49 -0700

    user stuff

--- src/kernel/obj/logical/user/user.c
+++ src/kernel/obj/logical/user/user.c
@@ -9,7 +9,7 @@
 #include <sys/kmalloc/kmalloc.h>
 #include <sys/kprintf/kprintf.h>
 #include <sys/obj/object/object.h>
-#include <sys/obj/objectinterface/objectinterface_group.h>
+#include <sys/obj/objectinterface/objectinterface_user.h>
 #include <sys/obj/objectmgr/objectmgr.h>
 #include <sys/obj/objecttype/objectype.h>
 #include <sys/string/mem.h>
@@ -17,9 +17,11 @@
 #include <types.h>
 
 #define USER_MAX_NAME 32
+#define USER_MAX_PWD 32
 
 struct user_objectdata {
     uint8_t name[USER_MAX_NAME];
+    uint8_t pwd[USER_MAX_PWD];
 };
 
 /*
@@ -45,6 +47,51 @@ uint8_t user_uninit(struct object* obj) {
     return 1;
 }
 
+void user_get_name(struct object* obj, uint8_t* name, uint16_t size) {
+    ASSERT_NOT_NULL(obj);
+    ASSERT_NOT_NULL(obj->object_data);
+    ASSERT_NOT_NULL(name);
+    ASSERT(size >= USER_MAX_NAME);
+    struct user_objectdata* object_data = (struct user_objectdata*)obj->object_data;
+    strncpy(name, object_data->name, size);
+}
+
+void user_set_name(struct object* obj, uint8_t* name, uint16_t size) {
+    ASSERT_NOT_NULL(obj);
+    ASSERT_NOT_NULL(obj->object_data);
+    ASSERT_NOT_NULL(name);
+    ASSERT(size <= USER_MAX_NAME);
+    struct user_objectdata* object_data = (struct user_objectdata*)obj->object_data;
+    strncpy(object_data->name, name, size);
+}
+
+void user_set_pwd(struct object* obj, uint8_t* name, uint8_t* pwd, uint16_t pwd_size) {
+    ASSERT_NOT_NULL(obj);
+    ASSERT_NOT_NULL(obj->object_data);
+    ASSERT_NOT_NULL(name);
+    ASSERT_NOT_NULL(pwd);
+    ASSERT(pwd_size <= USER_MAX_PWD);
+    struct user_objectdata* object_data = (struct user_objectdata*)obj->object_data;
+
+    if (0 == strcmp(object_data->name, name)) {
+        strncpy(object_data->pwd, pwd, pwd_size);
+    }
+}
+
+uint8_t user_validate_pwd(struct object* obj, uint8_t* name, uint8_t* pwd) {
+    ASSERT_NOT_NULL(obj);
+    ASSERT_NOT_NULL(obj->object_data);
+    ASSERT_NOT_NULL(name);
+    ASSERT_NOT_NULL(pwd);
+    struct user_objectdata* object_data = (struct user_objectdata*)obj->object_data;
+    if (0 == strcmp(object_data->name, name)) {
+        if (0 == strcmp(object_data->pwd, pwd)) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
 struct object* user_attach(uint8_t* name) {
     ASSERT_NOT_NULL(name);
     ASSERT(strlen(name) < USER_MAX_NAME);
@@ -60,9 +107,12 @@ struct object* user_attach(uint8_t* name) {
     /*
      * the device api
      */
-    struct objectinterface_group* api = (struct objectinterface_group*)kmalloc(sizeof(struct objectinterface_group));
-    memzero((uint8_t*)api, sizeof(struct objectinterface_group));
-    //    api->read = &user_read;
+    struct objectinterface_user* api = (struct objectinterface_user*)kmalloc(sizeof(struct objectinterface_user));
+    memzero((uint8_t*)api, sizeof(struct objectinterface_user));
+    api->get_name = &user_get_name;
+    api->set_name = &user_set_name;
+    api->set_pwd = &user_set_pwd;
+    api->validate_pwd = &user_validate_pwd;
     objectinstance->api = api;
     /*
      * device data
--- src/kernel/sys/obj/objectinterface/objectinterface_user.h
+++ src/kernel/sys/obj/objectinterface/objectinterface_user.h
@@ -13,9 +13,15 @@
 #include <types.h>
 
 typedef void (*user_get_name_function)(struct object* obj, uint8_t* name, uint16_t size);
+typedef void (*user_set_name_function)(struct object* obj, uint8_t* name, uint16_t size);
+typedef void (*user_set_pwd_function)(struct object* obj, uint8_t* name, uint8_t* pwd, uint16_t pwd_size);
+typedef uint8_t (*user_validate_pwd_function)(struct object* obj, uint8_t* name, uint8_t* pwd);
 
 struct objectinterface_user {
-    user_get_name_function name;
+    user_get_name_function get_name;
+    user_set_name_function set_name;
+    user_set_pwd_function set_pwd;
+    user_validate_pwd_function validate_pwd;
 };
 
 #endif
