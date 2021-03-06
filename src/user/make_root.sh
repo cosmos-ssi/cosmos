f9bf64671b12f233846c6281d129fa5779f75c5f
Tom Everett <tom@khubla.com>
2021-03-06 14:18:29 -0700

    parition fun

--- src/user/make_root.sh
+++ src/user/make_root.sh
@@ -6,10 +6,19 @@ DD=dd
 GPT=gpt
 MKFS=/usr/local/Cellar/dosfstools/4.2/sbin/mkfs.msdos
 
+# 3200 * 32768 = 100 MB
 $DD if=/dev/zero of=$IMAGE bs=32768 count=3200
+
+# make a GPT partion table
 $GPT create $IMAGE
-$GPT add -t windows $IMAGE
+
+# windows parition. 512 byte sectors, so 80MB is 163840 sectors
+$GPT add -t windows -s 163840 $IMAGE
+
+# a cosmos swap partition, uses the rest of the disk which is about 10MB
+GPT add -t 3c453e36-0c03-4b8b-a88f-2228caf8f2b1 $IMAGE
+
 $GPT show $IMAGE
 
-# msdos partition starts at sector 34
-$MKFS --offset 34 $IMAGE
+# msdos FAT 16 partition starts at sector 34
+$MKFS -F 16 -n COSMOS --offset 34 $IMAGE
