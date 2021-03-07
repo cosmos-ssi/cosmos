d78ed2593eb31e01b6fa206a2d891139d9ce9a25
Tom Everett <tom@khubla.com>
2021-03-07 14:32:56 -0700

    start of ls

---
+++ src/user/apps/ls/ls.cpp
@@ -0,0 +1,18 @@
+//*****************************************************************
+// This file is part of CosmOS                                    *
+// Copyright (C) 2020-2021 Tom Everett                            *
+// Released under the stated terms in the file LICENSE            *
+// See the file "LICENSE" in the source distribution for details  *
+// ****************************************************************
+
+#include <ls.hpp>
+
+ls::ls() {}
+
+void ls::run() {}
+
+int main(int argc, char* argv[]) {
+    //   Term* term = new Term();
+    // term->run();
+    return 0;
+}
---
+++ src/user/apps/ls/ls.hpp
@@ -0,0 +1,26 @@
+//*****************************************************************
+// This file is part of CosmOS                                    *
+// Copyright (C) 2020-2021 Tom Everett                            *
+// Released under the stated terms in the file LICENSE            *
+// See the file "LICENSE" in the source distribution for details  *
+// ****************************************************************
+
+/*
+* ls
+*/
+
+#ifndef _LS_HPP
+#define _LS_HPP
+
+#include <cosmos.hpp>
+
+class ls {
+  private:
+  public:
+    ls();
+    void run();
+};
+
+int main(int argc, char* argv[]);
+
+#endif
