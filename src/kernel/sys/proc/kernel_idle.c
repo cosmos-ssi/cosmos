b1842c60850a014429ab5accbb38b57f17612e29
Kurt M. Weber <weberk294@gmail.com>
2021-03-08 14:20:41 -0600

    forgot one

--- src/kernel/sys/proc/kernel_idle.c
+++ src/kernel/sys/proc/kernel_idle.c
@@ -12,8 +12,6 @@ void* kernel_idle(void* arg) {
     // We don't actually use the argument, but the kernel_work object requires
     // that the function take one, so it's probably best to just pass it NULL.
 
-    kprintf("Kernel idle!\n");
-
     while (1) {
         asm("hlt");
     }
