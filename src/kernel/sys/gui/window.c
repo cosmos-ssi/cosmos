9db62720abe151df4ec627d798c5f8de745df492
Tom Everett <tom@khubla.com>
2021-03-14 17:29:22 -0600

    colors

--- src/kernel/sys/gui/window.c
+++ src/kernel/sys/gui/window.c
@@ -17,10 +17,10 @@ struct window* window_new(uint32_t x, uint32_t y, uint32_t width, uint32_t heigh
     ret->titlebar_height = 20;
     window_set_position(ret, x, y);
     window_set_dimensions(ret, width, height);
-    ret->background_color = 0x777777;
-    ret->title_background_color = 0x052433;  // dark blue
-    ret->border_color = 0x052433;            // dark blue
-    ret->title_font_color = 0xDDDDDD;        // white-ish
+    ret->background_color = 0xFFFFFF;
+    ret->title_background_color = 0xD8E1E7;
+    ret->border_color = 0xD8E1E7;
+    ret->title_font_color = 0x2A2E29;
     strncpy(ret->title, "CosmOS", WINDOW_MAX_TITLE);
     return ret;
 }
@@ -29,6 +29,7 @@ void window_render(struct window* wind, struct canvas* cvs) {
     ASSERT_NOT_NULL(wind);
     ASSERT_NOT_NULL(cvs);
     //  kprintf("x %llu, y %llu, width %llu, height %llu\n", wind->x, wind->y, wind->width, wind->height);
+
     canvas_draw_line(cvs, wind->x, wind->y, wind->x + wind->width, wind->y, wind->border_color);
     canvas_draw_line(cvs, wind->x, wind->y + wind->titlebar_height, wind->x + wind->width,
                      wind->y + wind->titlebar_height, wind->border_color);
@@ -37,10 +38,14 @@ void window_render(struct window* wind, struct canvas* cvs) {
     canvas_draw_line(cvs, wind->x, wind->y, wind->x, wind->y + wind->height, wind->border_color);
     canvas_draw_line(cvs, wind->x + wind->width, wind->y, wind->x + wind->width, wind->y + wind->height,
                      wind->border_color);
+
+    // fill titlebar
     canvas_fill(cvs, wind->x + 1, wind->y + wind->titlebar_height + 1, wind->x + wind->width - 1,
                 wind->y + wind->height - 1, wind->background_color);
+    // fill body
     canvas_fill(cvs, wind->x + 1, wind->y + 1, wind->x + wind->width - 1, wind->y + wind->titlebar_height - 1,
                 wind->title_background_color);
+    // draw title
     canvas_draw_letters(cvs, gui_state->font, wind->x + 2, wind->y + 2, wind->title, wind->title_font_color);
 }
 
