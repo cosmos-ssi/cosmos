bc4f42c8bedb4c2c920ec7177205d017e7d692b2
Tom Everett <tom@khubla.com>
2021-03-21 14:14:30 -0600

    cleanup

--- src/user/fs/rootfs/test.asm
+++ src/user/fs/rootfs/test.asm
@@ -2,9 +2,6 @@
 
 %include 'fs/rootfs/syscall.asm'
 
-mov rax, 1
-add rax, 5
-
 push 55
 
 mov rax, SYSCALL_CONSOLE_WRITE
@@ -14,9 +11,6 @@ syscall
 mov rax, SYSCALL_PROCESS_EXIT
 mov rbx, rsp
 syscall
-
-mov rax, 6
-add rax, 9
 
 loop:
 jmp loop
