33d8bae11ff76e50c91c42a656642cb9011ac387
Tom Everett <tom@khubla.com>
2021-03-22 18:07:19 -0600

    updated clean script

--- src/clean.sh
+++ src/clean.sh
@@ -3,5 +3,7 @@
 echo "cleaning all generated files"
 find . -type f -name '*.o' -exec rm {} +
 find . -type f -name '*.a' -exec rm {} +
+find . -type f -name '*.ar' -exec rm {} +
 find . -type f -name '*.bin' -exec rm {} +
 find . -type f -name '*.elf' -exec rm {} +
+find . -type f -name '*.map' -exec rm {} +
