8587fe4ba875f4abb59c42916362875b94846ac0
Tom Everett <tom@khubla.com>
2021-03-22 19:00:28 -0600

    fix

--- src/kernel/sys/syscall/syscalls_objectmgr.h
+++ src/kernel/sys/syscall/syscalls_objectmgr.h
@@ -8,6 +8,7 @@
 #ifndef _SYSCALLS_OBJMGR_H
 #define _SYSCALLS_OBJMGR_H
 
+#include <sys/obj/object/object.h>
 #include <types.h>
 
 uint64_t syscall_objectmgr_get_device_by_name(uint64_t syscall_id, uint64_t arg1, uint64_t arg2, uint64_t arg3);
