786c1f7e3cc53897761be3f9bd3783542e428603
Tom Everett <tom@khubla.com>
2021-03-25 19:13:30 -0600

    cheap hack

--- src/kernel/sys/syscall/syscalls_serial.c
+++ src/kernel/sys/syscall/syscalls_serial.c
@@ -10,8 +10,10 @@
 #include <sys/obj/objectinterface/objectinterface_serial.h>
 #include <sys/obj/objectmgr/objectmgr.h>
 
+#define SYSCALL_SERIAL_DEVICE "serial0"
+
 uint64_t syscall_serial_readchar(uint64_t syscall_id, uint64_t arg1, uint64_t arg2, uint64_t arg3) {
-    struct object* serial_obj = objectmgr_find_object_by_name("serial0");
+    struct object* serial_obj = objectmgr_find_object_by_name(SYSCALL_SERIAL_DEVICE);
     if (0 != serial_obj) {
         struct objectinterface_serial* api = (struct objectinterface_serial*)serial_obj->api;
         return (*api->readchar)(serial_obj);
@@ -19,7 +21,7 @@ uint64_t syscall_serial_readchar(uint64_t syscall_id, uint64_t arg1, uint64_t ar
     return 0;
 }
 uint64_t syscall_serial_writechar(uint64_t syscall_id, uint64_t arg1, uint64_t arg2, uint64_t arg3) {
-    struct object* serial_obj = objectmgr_find_object_by_name("serial0");
+    struct object* serial_obj = objectmgr_find_object_by_name(SYSCALL_SERIAL_DEVICE);
     if (0 != serial_obj) {
         struct objectinterface_serial* api = (struct objectinterface_serial*)serial_obj->api;
         (*api->writechar)(serial_obj, arg1);
