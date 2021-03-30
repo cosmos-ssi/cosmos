4ec6c72fabab2f1ce3bb793c29162f0fbfee97f8
Tom Everett <tom@khubla.com>
2021-03-29 18:17:48 -0600

    change header

--- src/user/lib/cosmos.hpp
+++ src/user/lib/cosmos.hpp
@@ -10,9 +10,14 @@
 
 extern "C" {
 #include <malloc.h>
+#include <mem.h>
+#include <start.h>
 #include <types.h>
 }
 #include <assert.hpp>
+#include <collection/array.hpp>
+#include <collection/arraylist.hpp>
+#include <collection/tree.hpp>
 #include <heap.hpp>
 #include <new.hpp>
 #include <object/objects.hpp>
