add6556ef6058848b2934072b2cfcce4d7649800
Tom Everett <tom@khubla.com>
2021-04-04 21:42:11 -0600

    better

--- src/user/lib/abi/syscall.asm
+++ src/user/lib/abi/syscall.asm
@@ -9,10 +9,28 @@ global syscall;
 
 syscall:
     push rdi        ; preserve rdi   
+    push rdx
+    push rcx
+    push rbx
+    push rsi
+    push r10
+    push r9
+    push r8
 
     mov rax, rdi    ; 1st param from userland function is in rdi: the syscall #
     mov rdi, rsi    ; put the 2nd param from userland function into rbx
+    mov rdx, 0
+    mov r10, 0
+    mov r8, 0
+    mov r9, 0
     syscall
 
+    pop r8
+    pop r9
+    pop r10
+    pop rsi
+    pop rbx
+    pop rcx
+    pop rdx
     pop rdi         ; restore rdi
     ret             ; return value is in rax
