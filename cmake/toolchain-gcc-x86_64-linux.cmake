bf11e8c5afa86d48e3149fecd38a594be9b3ee30
Jeff Schnurr <jschnurr@gmail.com>
2021-04-11 14:49:17 -0400

    updates

--- cmake/toolchain-gcc-x86_64-linux.cmake
+++ cmake/toolchain-gcc-x86_64-linux.cmake
@@ -14,30 +14,13 @@ if($TC_PATH)
 endif()
 
 # The toolchain prefix for all toolchain executables
-set(CROSS_COMPILE)
+set(CROSS_COMPILE x86_64-linux-gnu-)
 
 # specify the cross compiler
 set(CMAKE_C_COMPILER ${CROSS_COMPILE}gcc)
 set(CMAKE_ASM_NASM_COMPILER nasm)
 
-# Because the cross-compiler cannot directly generate a binary without complaining, just test
-# compiling a static library instead of an executable program
-set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
-
-# We must set the OBJCOPY setting into cache so that it's available to the
-# whole project. Otherwise, this does not get set into the CACHE and therefore
-# the build doesn't know what the OBJCOPY filepath is
-
-set(CMAKE_OBJCOPY
-    ${TC_PATH}${CROSS_COMPILE}objcopy
-    CACHE FILEPATH "The toolchain objcopy command " FORCE
-)
-
-set(CMAKE_OBJDUMP
-    ${TC_PATH}${CROSS_COMPILE}objdump
-    CACHE FILEPATH "The toolchain objdump command " FORCE
-)
-
+# prevent find from looking for programs outside our toolchain
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
