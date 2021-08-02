321cd46404a96e2e779006a3c26ea4ac416e006a
Kurt M. Weber <weberk294@gmail.com>
2021-08-02 17:01:21 -0500

    new acpi subsystem

---
+++ src/kernel/sys/acpi/acpi.c
@@ -0,0 +1,10 @@
+/*****************************************************************
+ * This file is part of CosmOS                                   *
+ * Copyright (C) 2019-2021 Kurt M. Weber                         *
+ * Released under the stated terms in the file LICENSE           *
+ * See the file "LICENSE" in the source distribution for details *
+ *****************************************************************/
+
+void acpi_init() {
+    return;
+}
