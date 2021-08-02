670ff3879334a6c9d7e328400d3f333e31a74fe4
Kurt M. Weber <weberk294@gmail.com>
2021-08-02 17:13:16 -0500

    RSDP signature

---
+++ src/kernel/sys/acpi/xsdt.h
@@ -0,0 +1,13 @@
+/*****************************************************************
+ * This file is part of CosmOS                                   *
+ * Copyright (C) 2019-2021 Kurt M. Weber                         *
+ * Released under the stated terms in the file LICENSE           *
+ * See the file "LICENSE" in the source distribution for details *
+ *****************************************************************/
+
+#ifndef _XSDT_H
+#define _XSDT_H
+
+#define RSDP_SIGNATURE "RSD PTR "
+
+#endif
