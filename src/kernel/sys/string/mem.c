9836d7be0a11cf0cc42208c93113c791195ba706
Kurt M. Weber <weberk294@gmail.com>
2021-08-02 19:56:22 -0500

    added memcmp()

--- src/kernel/sys/string/mem.c
+++ src/kernel/sys/string/mem.c
@@ -9,6 +9,44 @@
 
 #include <sys/debug/assert.h>
 #include <sys/string/mem.h>
+#include <types.h>
+
+int8_t memcmp(const void* s1, const void* s2, size_t n) {
+    ASSERT_NOT_NULL(s1);
+    ASSERT_NOT_NULL(s2);
+    ASSERT_NOT_NULL(n);
+
+    uint64_t i;
+
+    /* 
+       Return 0 if first n elements at s1 are equal to first n elements at s2.
+
+       Return -1 if the first element of s1 that differs from its corresponding
+       element of s2, is less than the element in s2; return 1 if it is greater
+       than.
+    */
+
+    for (i = 0; i < n; i++) {
+        if (((BYTE*)s1)[i] == ((BYTE*)s2)[i]) {
+            // If they're equivalent then continue to next element.
+            continue;
+        }
+
+        /*
+           If they're unequal, then determine how they're unequal and return the
+           proper value.  One inequality means the strings are not equal, so we
+           can return immediately.
+        */
+
+        return ((((BYTE*)s1)[i] > ((BYTE*)s2)[i]) ? 1 : -1);
+    }
+
+    /* 
+       If we reach this point then there have been no elements that differ, so
+       return 0 for equal
+    */
+    return 0;
+}
 
 uint8_t* memcpy(uint8_t* restrict dstptr, const uint8_t* restrict srcptr, uint64_t size) {
     ASSERT_NOT_NULL(dstptr);
