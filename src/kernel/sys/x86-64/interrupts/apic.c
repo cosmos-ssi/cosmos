0fb27d034ffaf7f2b3f84dcb5a11311d15a58953
Kurt M. Weber <weberk294@gmail.com>
2021-08-08 13:07:43 -0500

    get base address

--- src/kernel/sys/x86-64/interrupts/apic.c
+++ src/kernel/sys/x86-64/interrupts/apic.c
@@ -4,3 +4,16 @@
  * Released under the stated terms in the file LICENSE           *
  * See the file "LICENSE" in the source distribution for details *
  *****************************************************************/
+
+#include <sys/debug/assert.h>
+#include <sys/x86-64/acpi/madt.h>
+
+void* apic_register_base;
+
+void apic_init() {
+    apic_register_base = acpi_get_local_apic_address();
+
+    ASSERT_NOT_NULL(apic_register_base);
+
+    return;
+}
