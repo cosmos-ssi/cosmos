cb9b1b384043f52e9cf959c33a670ff9d2e89612
Kurt M. Weber <weberk294@gmail.com>
2021-08-09 12:23:04 -0500

    MADT record types

--- src/kernel/sys/x86-64/acpi/madt.h
+++ src/kernel/sys/x86-64/acpi/madt.h
@@ -13,6 +13,29 @@
 #include <sys/x86-64/interrupts/apic.h>
 #include <types.h>
 
+// per https://uefi.org/specs/ACPI/6.4/05_ACPI_Software_Programming_Model/ACPI_Software_Programming_Model.html#i-o-apic-structure
+typedef enum acpi_madt_record_types_t {
+    ACPI_MADT_RECORD_PROCESSOR_LAPIC = 0,  // be careful that you don't confuse this with LSAPIC below
+    ACPI_MADT_RECORD_IOAPIC =
+        1,  // be careful especially if using an editor with auto-complete, this is very similar to IOSAPIC below
+    ACPI_MADT_RECORD_INTERRUPT_SOURCE_OVERRIDE = 2,
+    ACPI_MADT_RECORD_NMI_SOURCE = 3,
+    ACPI_MADT_RECORD_LAPIC_NMI = 4,
+    ACPI_MADT_RECORD_LAPIC_ADDRESS_OVERRIDE = 5,
+    ACPI_MADT_RECORD_IOSAPIC =
+        6,  // be careful especially if using an editor with auto-complete, this is very similar to IOAPIC above
+    ACPI_MADT_RECORD_PROCESSOR_LSAPIC = 7,  // be careful that you don't confuse this with LAPIC above
+    ACPI_MADT_RECORD_PLATFORM_INTERRUPT_SOURCE = 8,
+    ACPI_MADT_RECORD_PROCESSOR_LX2APIC = 9,
+    ACPI_MADT_RECORD_PROCESSOR_LX2APIC_NMI = 10,
+    ACPI_MADT_RECORD_GICC = 11,
+    ACPI_MADT_RECORD_GICD = 12,
+    ACPI_MADT_RECORD_GIC_MSI_FRAME = 13,
+    ACPI_MADT_RECORD_GICR = 14,
+    ACPI_MADT_RECORD_GIC_ITS = 15,
+    ACPI_MADT_RECORD_MULTIPROCESSOR_WAKEUP = 16
+} acpi_madt_record_types_t;
+
 typedef struct acpi_madt_t {
     acpi_sdt_header_t header;
     uint32_t local_apic_address;
